9f9979255715c0b17d91c71ef4d37385f03677cc
chenzhe142 <chenzhe142@163.com>
2016-12-02 22:53:29 -0800

    show 'added by me' tag for tea notes

--- src/components/ImageRow.js
+++ src/components/ImageRow.js
@@ -28,6 +28,7 @@ import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import containers from '../style/containers';
 import color from '../style/color';
+import colorScheme from '../style/colorScheme';
 import text from '../style/text';
 
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../constants';
@@ -36,16 +37,21 @@ export default class ImageRow extends Component {
   render() {
     let addedByMe;
     if (this.props.tea.addedByMe) {
-      addedByMe = <Text>#added by me</Text>;
+      addedByMe =
+      <View style={{position: 'absolute', top: 8, left: 8, borderRadius: 2, backgroundColor: 'rgba(244,139,148,0.9)', height: 25, width: 105}}>
+        <View style={[containers.container, {justifyContent: 'center', alignItems: 'center'}]}>
+          <Text style={[text.subTitle, {color: color.white, fontWeight: '400', backgroundColor: 'rgba(0,0,0,0)'}]}>Added by me</Text>
+        </View>
+      </View>
     }
     return (
       <View>
         <TouchableWithoutFeedback onPress={this.props.onPressEvent}>
           <View style={{paddingBottom: 1}}>
             <Image style={styles.image} source={this.props.imageSource}>
+              {addedByMe}
               <View style={[containers.container, {justifyContent: 'center', alignItems: 'center'}]}>
                 <Text style={[text.title, text.shadow, {color: color.white, fontWeight: '700', backgroundColor: 'rgba(0,0,0,0)'}]}>{this.props.tea.name}</Text>
-                {addedByMe}
               </View>
             </Image>
           </View>
